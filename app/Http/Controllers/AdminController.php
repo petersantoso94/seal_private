3ef0c595522cdb7645c19ceb07066beb69bc793c
petersantoso94 <petersantoso94@gmail.com>
2018-05-15 22:44:37 +0800

    15-5-2018

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-class AdminController extends Controller
-{
+class AdminController extends Controller {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
 
@@ -21,12 +21,18 @@ class AdminController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$username = INPUT::get('email');
-	$username = INPUT::get('email');
-}
+    public function index() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $username = INPUT::get('email');
+            $pass = INPUT::get('password');
+            $pass = 'admin123';
+            $pass = Hash::make($pass);
+            dd($pass);
+            if($username === 'admin-cos'){
+                
+            }
+        }
         return view('home');
     }
+
 }
