9995b71b09729b7fd8178b17b26f01e2db7073ae
petersantoso94 <petersantoso94@gmail.com>
2018-05-16 20:22:24 +0800

    16-5-2018

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,29 @@
 @extends('template.header-footer-admin')
 @section('main-section')
+<div class="white-pane__bordered margbot20">
+    <table id="example" class="display table-rwd table-inventory" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Facebook Name</th>
+                <th>Email</th>
+                <th>Action</th>
+            <!--<th>Actions</th>-->
+            </tr>
+        </thead>
+    </table>
+</div>
+@endsection
+@section('js-content')
+<script>
+    var table = '';
+    var registerDataTable = '<?php echo Route('registerDataTable') ?>';
+    table = $('#example').dataTable({
+        "draw": 10,
+        "processing": true,
+        "bDestroy": true,
+        "serverSide": true,
+        "ajax": registerDataTable
+    });
+</script>
 @endsection
