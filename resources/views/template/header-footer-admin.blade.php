0c11702420ab4298733e1632be2894383ab584c4
petersantoso94 <petersantoso94@gmail.com>
2018-05-16 20:29:40 +0800

    16-5-2018

--- resources/views/template/header-footer-admin.blade.php
+++ resources/views/template/header-footer-admin.blade.php
@@ -11,7 +11,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <title>AdminLTE 2 | Starter</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <link rel="stylesheet" type="text/css" href="{{Asset('lib/bootstrap/css/jquery.dataTables.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('public/jquery.dataTables.css')}}">
         <link href="{{ URL::asset('public/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ URL::asset('public/bower_components/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
         <link href="{{ URL::asset('public/bower_components/Ionicons/css/ionicons.min.css') }}" rel="stylesheet">
