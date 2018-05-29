bd57bf0ade805d719cebab1e5663e6b10b5a923d
petersantoso94 <petersantoso94@gmail.com>
2018-05-29 22:13:35 +0800

    send cash 28-5-2018

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -86,8 +86,8 @@ class AdminController extends Controller {
             }
             $registered_id = DB::connection('mysql')->table($table)->select('id')->where('id', $a)->get();
             if(count($registered_id) > 0){
-                $registered_id->point = $registered_id->point + $cash;
-                $registered_id->save();
+                $registered_id[0]->point = $registered_id[0]->point + $cash;
+                $registered_id[0]->save();
             }
         }
     }
