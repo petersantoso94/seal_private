3136ff4b77ecc6da65905517dbeba47e69d52e93
petersantoso94 <petersantoso94@gmail.com>
2018-05-29 22:02:26 +0800

    send cash 28-5-2018

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -48,8 +48,8 @@ class AdminController extends Controller {
         $request->session()->forget('admin');
         return redirect('adminpanelcos');
     }
-    
-    public function sendcash(Request $request){
+
+    public function sendcash(Request $request) {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
         }
@@ -60,8 +60,37 @@ class AdminController extends Controller {
         }
         return view('admin.sendcash')->withPage('Send Cash');
     }
-    
-    public function postValid(Request $request){
+
+    public function postCash(Request $request) {
+        $ids = $request->get('users');
+        $ids = explode(',', $ids);
+        $cash = $request->get('nominal');
+        foreach ($ids as $id) {
+            $a = $id;
+            $letter = $a['0'];
+            $table = '';
+            if (preg_match("/[aA-dD0-9]/", $letter)) {
+                $table = "idtable1";
+            } else if (preg_match("/[eE-iI]/", $letter)) {
+                $table = "idtable2";
+            } else if (preg_match("/[eJ-nN]/", $letter)) {
+                $table = "idtable3";
+            } else if (preg_match("/[oO-sS]/", $letter)) {
+                $table = "idtable4";
+            } else if (preg_match("/[tT-zZ]/", $letter)) {
+                $table = "idtable5";
+            } else {
+                $table = "idtable5";
+            }
+            $registered_id = DB::connection('mysql')->table($table)->select('id')->where('id', $a)->get();
+            if(count($registered_id) > 0){
+                $registered_id->point = $registered_id->point + $cash;
+                $registered_id->save();
+            }
+        }
+    }
+
+    public function postValid(Request $request) {
         $id = $request->get('sn');
         //$id = 'gamelemah2';
         $a = $id;
@@ -80,21 +109,21 @@ class AdminController extends Controller {
         } else {
             $table = "idtable5";
         }
-        
+
         $registered_id = DB::connection('mysql')->table($table)->select('id')->where('id', $a)->get();
         $new_id = DB::connection('mysql2')->table('idtable1')->select('*')->where('id', $a)->get();
-		$new_id = $new_id[0];
+        $new_id = $new_id[0];
         if (count($registered_id) == 0) {
             DB::connection('mysql')->insert("INSERT INTO {$table} VALUES('{$a}','{$new_id->passwd}',CURDATE(),'99','','0',NULL,'',0,0,NULL,NULL,0,NULL,0,CURDATE(),'{$new_id->nick_name}','','{$new_id->email}','{$new_id->trueId}',0,0,0,0,0,'{$new_id->fb_acc}','{$new_id->recom}')");
-			DB::connection('mysql2')->delete("DELETE FROM idtable1 WHERE id = '".$a."'");
-			DB::connection('mysql2')->delete("DELETE FROM users WHERE name = '".$a."'");
+            DB::connection('mysql2')->delete("DELETE FROM idtable1 WHERE id = '" . $a . "'");
+            DB::connection('mysql2')->delete("DELETE FROM users WHERE name = '" . $a . "'");
         }
     }
-	
-	public function postDelete(Request $request){
+
+    public function postDelete(Request $request) {
         $id = $request->get('sn');
-        DB::connection('mysql2')->delete("DELETE FROM idtable1 WHERE id = '".$id."'");
-			DB::connection('mysql2')->delete("DELETE FROM users WHERE name = '".$id."'");
+        DB::connection('mysql2')->delete("DELETE FROM idtable1 WHERE id = '" . $id . "'");
+        DB::connection('mysql2')->delete("DELETE FROM users WHERE name = '" . $id . "'");
     }
 
 }
