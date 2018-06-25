5366095d5c08d96dfc09ea745af473d155825657
petersantoso94 <petersantoso94@gmail.com>
2018-06-25 12:13:16 +0700

    carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,6 +49,36 @@
 
     <!-- Page Features -->
     <div class="row text-center">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	  <ol class="carousel-indicators">
+		<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+		<div class="carousel-item active">
+		  <img class="d-block w-100" src="{{URL::asset('public/picture/Instagram Details.png')}}" alt="First slide">
+		  <div class="carousel-caption d-none d-md-block">
+    <h5>...</h5>
+    <p>...</p>
+  </div>
+		</div>
+		<div class="carousel-item">
+		  <img class="d-block w-100" src="{{URL::asset('public/picture/Details.png')}}" alt="Second slide">
+		</div>
+		<div class="carousel-item">
+		  <img class="d-block w-100" src="{{URL::asset('public/picture/Open_Beta.jpg')}}" alt="Third slide">
+		</div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	  </a>
+	</div>
         <div class="col">
 		<div class="card">
 				<br />
