43fbd68ee638ca4ae6d4c4b5f020e1922ce70f52
petersantoso94 <petersantoso94@gmail.com>
2018-06-26 12:36:24 +0800

    admin panel 26-6-2018

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,7 +49,7 @@
 
     <!-- Page Features -->
     <div class="row text-center">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="max-height:800px;">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
