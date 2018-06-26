135e68b42ad22baf64ad6ee7ef03fab25bb0438e
petersantoso94 <petersantoso94@gmail.com>
2018-06-26 15:49:04 +0800

    admin panel 26-6-2018

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallba
 Route::get('/logout', 'Auth\LoginController@logout');
 Route::any('loginadmin', array('as' => 'loginadmin', 'uses' => 'AdminController@index'));
 Route::any('sendcash', array('as' => 'sendcash', 'uses' => 'AdminController@sendcash'));
+Route::any('editpage', array('as' => 'editpage', 'uses' => 'AdminController@editpage'));
 Route::any('logoutadmin', array('as' => 'logoutadmin', 'uses' => 'AdminController@logout'));
 Route::any('postValid', array('as' => 'postValid', 'uses' => 'AdminController@postValid'));
 Route::any('postDelete', array('as' => 'postDelete', 'uses' => 'AdminController@postDelete'));
