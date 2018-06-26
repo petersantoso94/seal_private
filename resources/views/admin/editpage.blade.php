03b15b3dcacf5ad1057eef96f80955acf4a52f27
petersantoso94 <petersantoso94@gmail.com>
2018-06-26 15:49:02 +0800

    admin panel 26-6-2018

---
+++ resources/views/admin/editpage.blade.php
@@ -0,0 +1,35 @@
+@extends('template.header-footer-admin')
+@section('main-section')
+<div class="white-pane__bordered margbot20" style="margin-left: 20px;">
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Bootstrap WYSIHTML5
+                <small>Simple and fast</small>
+            </h3>
+            <!-- tools box -->
+            <div class="pull-right box-tools">
+                <button type="button" class="btn btn-default btn-sm" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Collapse">
+                    <i class="fa fa-minus"></i></button>
+                <button type="button" class="btn btn-default btn-sm" data-widget="remove" data-toggle="tooltip" title="" data-original-title="Remove">
+                    <i class="fa fa-times"></i></button>
+            </div>
+            <!-- /. tools -->
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body pad">
+            <form>
+                <textarea class="textarea" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid rgb(221, 221, 221); padding: 10px; display: none;" placeholder="Place some text here"></textarea>
+                <input type="hidden" name="_wysihtml5_mode" value="1">
+                <iframe class="wysihtml5-sandbox" security="restricted" allowtransparency="true" frameborder="0" width="0" height="0" marginwidth="0" marginheight="0" style="display: inline-block; background-color: rgb(255, 255, 255); border-collapse: separate; border-color: rgb(221, 221, 221); border-style: solid; border-width: 1px; clear: none; float: none; margin: 0px; outline: rgb(51, 51, 51) none 0px; outline-offset: 0px; padding: 10px; position: static; top: auto; left: auto; right: auto; bottom: auto; z-index: auto; vertical-align: baseline; text-align: start; box-sizing: border-box; box-shadow: none; border-radius: 0px; width: 100%; height: 200px;"></iframe>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
+@section('js-content')
+<script>
+    $(function () {
+        $('.textarea').wysihtml5();
+    });
+</script>
+@endsection
