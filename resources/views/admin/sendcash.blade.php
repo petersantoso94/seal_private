b0f8851b1f6eb499c7aabe731b2962ce77f87158
petersantoso94 <petersantoso94@gmail.com>
2018-06-26 11:56:06 +0800

    admin panel 26-6-2018

--- resources/views/admin/sendcash.blade.php
+++ resources/views/admin/sendcash.blade.php
@@ -1,81 +1,93 @@
 @extends('template.header-footer-admin')
 @section('main-section')
 <div class="white-pane__bordered margbot20" style="margin-left: 20px;">
-    <h3>Cash transfer</h3>
-    <form>
-        <div class="form-group">
-            <label for="users">User's Character Name:</label><br>
-            <select data-placeholder="Choose users..." class="chosen-select" name="users" id="users"  multiple="" tabindex="-1" >
-                <option></option>
-                @foreach(DB::connection('mysql3')->table('pc')->select('char_name','user_id')->get() as $char)
-                @if($char->char_name != '')
-                <option value="{{$char->user_id}}">
-                    {{$char->char_name}}
-                </option>
-                @endif
-                @endforeach
-            </select>
+    <div class='row'>
+        <div class='col-xs-8'>
+            <h3>Cash transfer</h3>
+            <form>
+                <div class="form-group">
+                    <label for="users">User's Character Name:</label><br>
+                    <select data-placeholder="Choose users..." class="chosen-select form-control" name="users" id="users"  multiple="" tabindex="-1" >
+                        <option></option>
+                        @foreach(DB::connection('mysql3')->table('pc')->select('char_name','user_id')->get() as $char)
+                        @if($char->char_name != '')
+                        <option value="{{$char->user_id}}">
+                            {{$char->char_name}}
+                        </option>
+                        @endif
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="cash-nominal">Cash Nominal:</label><br>
+                    <input type="number" id='cash-nominal' class='form-control'>
+                </div>
+                <button type="submit" class="btn btn-primary" onclick="sendCash(this)">Submit</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="cash-nominal">Cash Nominal:</label><br>
-            <input type="number" id='cash-nominal'>
-        </div>
-        <button type="submit" class="btn btn-primary" onclick="sendCash(this)">Submit</button>
-    </form>
+    </div>
 </div>
 <div class="white-pane__bordered margbot20" style="margin-left: 20px;">
-    <h3>Item transfer</h3>
-    <form>
-        <div class="form-group">
-            <label for="users">User's Character Name:</label><br>
-            <select data-placeholder="Choose users..." class="chosen-select form-control" name="users_for_item" id="users_for_item"  multiple="" tabindex="-1" >
-                <option></option>
-                @foreach(DB::connection('mysql3')->table('pc')->select('char_name','user_id')->get() as $char)
-                @if($char->char_name != '')
-                <option value="{{$char->user_id}}">
-                    {{$char->char_name}}
-                </option>
-                @endif
-                @endforeach
-            </select>
+    <div class='row'>
+        <div class='col-xs-8'>
+            <h3>Item transfer</h3>
+            <form>
+                <div class="form-group">
+                    <label for="users">User's Character Name:</label><br>
+                    <select data-placeholder="Choose users..." class="chosen-select form-control" name="users_for_item" id="users_for_item"  multiple="" tabindex="-1" >
+                        <option></option>
+                        @foreach(DB::connection('mysql3')->table('pc')->select('char_name','user_id')->get() as $char)
+                        @if($char->char_name != '')
+                        <option value="{{$char->user_id}}">
+                            {{$char->char_name}}
+                        </option>
+                        @endif
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="cash-nominal">Item Code:</label><br>
+                    <input type="number" id='val-it' class='form-control'>
+                </div>
+                <div class="form-group">
+                    <label for="cash-nominal">Item's Quantity / Refine Level:</label><br>
+                    <input type="number" id='val-io' class='form-control'>
+                </div>
+                <button type="submit" class="btn btn-primary" onclick="sendItems(this)">Submit</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="cash-nominal">Item Code:</label><br>
-            <input type="number" id='val-it' class='form-control'>
-        </div>
-        <div class="form-group">
-            <label for="cash-nominal">Item's Quantity / Refine Level:</label><br>
-            <input type="number" id='val-io' class='form-control'>
-        </div>
-        <button type="submit" class="btn btn-primary" onclick="sendItems(this)">Submit</button>
-    </form>
+    </div>
 </div>
 <div class="white-pane__bordered margbot20" style="margin-left: 20px;">
-    <h3>Item transfer (add)</h3>
-    <form>
-        <div class="form-group">
-            <label for="users">User's Character Name:</label><br>
-            <select data-placeholder="Choose users..." class="chosen-select form-control" name="users_for_item_add" id="users_for_item_add"  multiple="" tabindex="-1" >
-                <option></option>
-                @foreach(DB::connection('mysql3')->table('pc')->select('char_name','user_id')->get() as $char)
-                @if($char->char_name != '')
-                <option value="{{$char->user_id}}">
-                    {{$char->char_name}}
-                </option>
-                @endif
-                @endforeach
-            </select>
+    <div class='row'>
+        <div class='col-xs-8'>
+            <h3>Item transfer (add)</h3>
+            <form>
+                <div class="form-group">
+                    <label for="users">User's Character Name:</label><br>
+                    <select data-placeholder="Choose users..." class="chosen-select form-control" name="users_for_item_add" id="users_for_item_add"  multiple="" tabindex="-1" >
+                        <option></option>
+                        @foreach(DB::connection('mysql3')->table('pc')->select('char_name','user_id')->get() as $char)
+                        @if($char->char_name != '')
+                        <option value="{{$char->user_id}}">
+                            {{$char->char_name}}
+                        </option>
+                        @endif
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="cash-nominal">Item Type:</label><br>
+                    <input type="number" id='val-type' class='form-control'>
+                </div>
+                <div class="form-group">
+                    <label for="cash-nominal">Durability:</label><br>
+                    <input type="number" id='val-dr' class='form-control'>
+                </div>
+                <button type="submit" class="btn btn-primary" onclick="sendItemsAdd(this)">Submit</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="cash-nominal">Item Type:</label><br>
-            <input type="number" id='val-type' class='form-control'>
-        </div>
-        <div class="form-group">
-            <label for="cash-nominal">Durability:</label><br>
-            <input type="number" id='val-dr' class='form-control'>
-        </div>
-        <button type="submit" class="btn btn-primary" onclick="sendItemsAdd(this)">Submit</button>
-    </form>
+    </div>
 </div>
 
 @endsection
