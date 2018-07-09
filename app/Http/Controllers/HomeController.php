d5377147b626cdb65075bada3e4104cf1e78c620
petersantoso94 <petersantoso94@gmail.com>
2018-07-09 14:50:11 +0800

    9-7-2018

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use DB;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         //$this->middleware('auth');
     }
 
@@ -23,14 +22,38 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         return view('home');
     }
-    
-    public function browse($id)
-    {
-		$data = DB::connection('mysql2')->table('content')->where('id', $id)->first();
-        return view('event')->with('eventdata',$data);
+
+    public function login(Request $request) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $rule = array(
+                'email' => 'required',
+                'password' => 'required'
+            );
+
+            $data = Input::all();
+            $validator = Validator::make($data, $rule);
+            if ($validator->fails()) {
+                return View::make('auth.login')
+                                ->withMessages('salahLogin')
+                                ->withErrors($validator->messages());
+            } else {
+                $email = Input::get('email');
+                $password = Input::get('password');
+                if (Auth::attempt(array('email' => $email, 'password' => $password), true)) {
+                    dd('masuk');
+                }
+            }
+            return View::make('auth.login')
+                            ->withMessages('gagalLogin');
+        }
     }
+
+    public function browse($id) {
+        $data = DB::connection('mysql2')->table('content')->where('id', $id)->first();
+        return view('event')->with('eventdata', $data);
+    }
+
 }
