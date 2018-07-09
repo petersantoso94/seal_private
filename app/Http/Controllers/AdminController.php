245d06706004ab85a6a3cd7722df3be3f85a7cfa
petersantoso94 <petersantoso94@gmail.com>
2018-07-09 15:02:33 +0800

    9-7-2018

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -252,7 +252,7 @@ class AdminController extends Controller {
                 if (count($registered_id) == 0) {
                     DB::connection('mysql')->insert("INSERT INTO {$table} VALUES('{$a}','{$new_id->passwd}',CURDATE(),'99','','0',NULL,'',0,0,NULL,NULL,0,NULL,0,CURDATE(),'{$new_id->nick_name}','','{$new_id->email}','{$new_id->trueId}',0,0,0,0,0,'{$new_id->fb_acc}','{$new_id->recom}')");
                     DB::connection('mysql2')->delete("DELETE FROM idtable1 WHERE id = '" . $a . "'");
-                    DB::connection('mysql2')->delete("DELETE FROM users WHERE name = '" . $a . "'");
+//                    DB::connection('mysql2')->delete("DELETE FROM users WHERE name = '" . $a . "'");
                 }
             }
         }
