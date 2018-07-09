7f9c34b0834a623454ff4f6611386b13d434f62c
petersantoso94 <petersantoso94@gmail.com>
2018-07-09 16:04:54 +0800

    9-7-2018

--- app/Notifications/MailResetPasswordToken.php
+++ app/Notifications/MailResetPasswordToken.php
@@ -1,26 +1,28 @@
 <?php
-
+ 
 namespace App\Notifications;
-
+ 
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-
+ 
 class MailResetPasswordToken extends Notification
 {
     use Queueable;
-
+ 
+    public $token;
+ 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($token)
     {
-        //
+        $this->token = $token;
     }
-
+ 
     /**
      * Get the notification's delivery channels.
      *
@@ -31,7 +33,7 @@ class MailResetPasswordToken extends Notification
     {
         return ['mail'];
     }
-
+ 
     /**
      * Get the mail representation of the notification.
      *
@@ -41,21 +43,10 @@ class MailResetPasswordToken extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->subject("Reset your password")
+                    ->line("Hey, did you forget your password? Click the button to reset it.")
+                    ->action('Reset Password', url('password/reset', $this->token))
+                    ->line('Thank you');
     }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
-    }
+ 
 }
