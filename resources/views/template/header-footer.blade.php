ea9125af7ecf7f2f090c03eb8552bc9075eaae07
petersantoso94 <petersantoso94@gmail.com>
2018-07-10 13:05:53 +0800

    10-7-2018

--- resources/views/template/header-footer.blade.php
+++ resources/views/template/header-footer.blade.php
@@ -106,37 +106,45 @@
             </div>
         </nav>
         <div id="wrap" style="">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
             <div class="container" id="main">
                 <div class="text-center" style="margin-top: 30px;background: #6c757d">
 
                     <div class="dropdown show" style="margin-left:auto;margin-right:auto;">
                         <!--dropdown-->
-                            <?php
-                            $horizontal = 0;
-                            $max_horizontal = DB::connection('mysql2')->table('content')->select('horizontal_level')->orderBy('horizontal_level', 'DESC')->first();
-                            if ($max_horizontal)
-                                $horizontal = $max_horizontal->horizontal_level;
-                            ?>
-                            @for($i = 1; $i<=$horizontal ;$i++)
-                            <?php $vertical = DB::connection('mysql2')->table('content')->orderBy('vertical_level', 'ASC')->where('horizontal_level', $i)->get(); ?>
-                            <div class="btn-group">
-                                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink<?php echo $i; ?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{$vertical[0]->horizontal_name}}
-                                </a>
+                        <?php
+                        $horizontal = 0;
+                        $max_horizontal = DB::connection('mysql2')->table('content')->select('horizontal_level')->orderBy('horizontal_level', 'DESC')->first();
+                        if ($max_horizontal)
+                            $horizontal = $max_horizontal->horizontal_level;
+                        ?>
+                        @for($i = 1; $i<=$horizontal ;$i++)
+                        <?php $vertical = DB::connection('mysql2')->table('content')->orderBy('vertical_level', 'ASC')->where('horizontal_level', $i)->get(); ?>
+                        <div class="btn-group">
+                            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink<?php echo $i; ?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{$vertical[0]->horizontal_name}}
+                            </a>
 
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink<?php echo $i; ?>">
-                                    @foreach($vertical as $data)
-                                    @if($data->link === '#')
-                                    <a class="dropdown-item" href="#">{{$data->name}}</a>
-                                    @elseif($data->content === '' || $data->content === NULL)
-                                    <a class="dropdown-item" href="{{$data->link}}">{{$data->name}}</a>
-                                    @else
-                                    <a class="dropdown-item" href="{{(url('browse').'/'.$data->id)}}">{{$data->name}}</a>
-                                    @endif
-                                    @endforeach
-                                </div>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink<?php echo $i; ?>">
+                                @foreach($vertical as $data)
+                                @if($data->link === '#')
+                                <a class="dropdown-item" href="#">{{$data->name}}</a>
+                                @elseif($data->content === '' || $data->content === NULL)
+                                <a class="dropdown-item" href="{{$data->link}}">{{$data->name}}</a>
+                                @else
+                                <a class="dropdown-item" href="{{(url('browse').'/'.$data->id)}}">{{$data->name}}</a>
+                                @endif
+                                @endforeach
                             </div>
-                            @endfor
+                        </div>
+                        @endfor
                     </div>
                 </div>
                 @yield('main-section')
