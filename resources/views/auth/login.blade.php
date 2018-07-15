5dafec3d2d3a933628ef568de0bd91bdac49242a
petersantoso94 <petersantoso94@gmail.com>
2018-07-15 13:30:55 +0800

    login manually 15-7-2018

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('loginmanual') }}">
                         @csrf
 
                         <div class="form-group row">
@@ -75,7 +75,7 @@
                                 @endif
                             </div>
                         </div>
-
+<!--
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
@@ -84,17 +84,17 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div>-->
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
+<!--
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
-                                </a>
+                                </a>-->
                             </div>
                         </div>
                     </form>
