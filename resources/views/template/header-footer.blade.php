8d1f5360731e4432a2fbb0201dd55200b578b965
petersantoso94 <petersantoso94@gmail.com>
2018-07-18 20:12:56 +0800

    18-7-2018

--- resources/views/template/header-footer.blade.php
+++ resources/views/template/header-footer.blade.php
@@ -65,7 +65,7 @@
                 z-index: 1; /* Sit on top */
                 left: 0;
                 top: 0;
-                width: 50%; /* Full width */
+                width: 100%; /* Full width */
                 height: 100%; /* Full height */
                 overflow: auto; /* Enable scroll if needed */
                 background-color: rgb(0,0,0); /* Fallback color */
@@ -78,7 +78,7 @@
                 background-color: #fefefe;
                 margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
                 border: 1px solid #888;
-                width: 80%; /* Could be more or less, depending on screen size */
+                width: 50%; /* Could be more or less, depending on screen size */
             }
 
             /* The Close Button (x) */
