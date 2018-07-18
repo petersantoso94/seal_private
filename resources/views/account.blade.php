27335dcdfa2974d5ef193e1c12a9f7764fb7babf
petersantoso94 <petersantoso94@gmail.com>
2018-07-18 21:48:50 +0800

    18-7-2018

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -4,53 +4,51 @@
 <!-- Jumbotron Header -->
 <!-- Page Features -->
 <div class="row" style="background: rgba(204, 204, 204, 0.8);margin-top: 20px;padding-left: 10px;padding-right: 10px">
-    <div class='col-xs-16'>
-        <form>
-            <h3>Welcome to SEAL ONLINE: Chronicles of Shiltz</h3>
-            <div class="form-group">
-                @if(Session::get('username') != null)
-                <label for="users">Hi, {{Session::get('username')}}</label><br>
-                @endif
-            </div>
-            <?php
-            $a = Session::get('username');
-            $email = '';
-            $pin = '';
-            $letter = $a['0'];
-            $table = '';
-            if (preg_match("/[aA-dD0-9]/", $letter)) {
-                $table = "idtable1";
-            } else if (preg_match("/[eE-iI]/", $letter)) {
-                $table = "idtable2";
-            } else if (preg_match("/[eJ-nN]/", $letter)) {
-                $table = "idtable3";
-            } else if (preg_match("/[oO-sS]/", $letter)) {
-                $table = "idtable4";
-            } else if (preg_match("/[tT-zZ]/", $letter)) {
-                $table = "idtable5";
-            } else {
-                $table = "idtable5";
-            }
-            $registered_id = DB::connection('mysql')->table($table)->select('*')->where('id', $a)->get();
-            if (count($registered_id) > 0) {
-                $email = $registered_id[0]->email;
-                $pin = $registered_id[0]->trueId;
-            }
-            ?>
-            <div class="form-group">
-                <label for="users">Your Email:</label><br>
-                <input type="text" id='email' class='form-control' value="{{$email}}" disabled="">
-                <button type="button" onclick="enableEmail(this)" id='btn-enable-email'>edit</button>
-                <button type="button" onclick="disableEmail(this)" id='btn-dis-email' style="display: none">cancel</button>
-            </div>
-            <div class="form-group">
-                <label for="users">Your PIN:</label><br>
-                <input type='password' class='form-control' id='pin' value='{{$pin}}' disabled="">
-                <button type="button" onclick="enablePin(this)" id='btn-enable-pin'>edit</button>
-                <button type="button" onclick="disablePin(this)" id='btn-dis-pin' style="display: none">cancel</button>
-            </div>
-        </form>
-    </div>
+    <form>
+        <h3>Welcome to SEAL ONLINE: Chronicles of Shiltz</h3>
+        <div class="form-group">
+            @if(Session::get('username') != null)
+            <label for="users">Hi, {{Session::get('username')}}</label><br>
+            @endif
+        </div>
+        <?php
+        $a = Session::get('username');
+        $email = '';
+        $pin = '';
+        $letter = $a['0'];
+        $table = '';
+        if (preg_match("/[aA-dD0-9]/", $letter)) {
+            $table = "idtable1";
+        } else if (preg_match("/[eE-iI]/", $letter)) {
+            $table = "idtable2";
+        } else if (preg_match("/[eJ-nN]/", $letter)) {
+            $table = "idtable3";
+        } else if (preg_match("/[oO-sS]/", $letter)) {
+            $table = "idtable4";
+        } else if (preg_match("/[tT-zZ]/", $letter)) {
+            $table = "idtable5";
+        } else {
+            $table = "idtable5";
+        }
+        $registered_id = DB::connection('mysql')->table($table)->select('*')->where('id', $a)->get();
+        if (count($registered_id) > 0) {
+            $email = $registered_id[0]->email;
+            $pin = $registered_id[0]->trueId;
+        }
+        ?>
+        <div class="form-group">
+            <label for="users">Your Email:</label>
+            <input type="text" id='email' class='form-control' value="{{$email}}" disabled="">
+            <button type="button" onclick="enableEmail(this)" id='btn-enable-email' class="btn btn-primary">edit</button>
+            <button type="button" onclick="disableEmail(this)" id='btn-dis-email' class="btn" style="display: none">cancel</button>
+        </div>
+        <div class="form-group">
+            <label for="users">Your PIN:</label>
+            <input type='password' class='form-control' id='pin' value='{{$pin}}' disabled="">
+            <button type="button" onclick="enablePin(this)" id='btn-enable-pin' class="btn btn-primary">edit</button>
+            <button type="button" onclick="disablePin(this)" id='btn-dis-pin' class='btn' style="display: none">cancel</button>
+        </div>
+    </form>
 </div>
 <!-- /.row -->
 
@@ -59,24 +57,24 @@
 @stop
 @section('js-content')
 <script>
-    var enableEmail = function(){
+    var enableEmail = function () {
         $(this).hide();
         $('#email').removeAttr('disabled');
         $('#btn-dis-email').show();
     };
-    var disableEmail = function(){
+    var disableEmail = function () {
         $(this).hide();
-        $('#email').attr('disabled','disabled');
+        $('#email').attr('disabled', 'disabled');
         $('#btn-enable-email').show();
     };
-    var enablePin = function(){
+    var enablePin = function () {
         $(this).hide();
         $('#pin').removeAttr('disabled');
         $('#btn-dis-pin').show();
     };
-    var disablePin = function(){
+    var disablePin = function () {
         $(this).hide();
-        $('#pin').attr('disabled','disabled');
+        $('#pin').attr('disabled', 'disabled');
         $('#btn-enable-pin').show();
     };
 </script>
