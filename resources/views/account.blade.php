25ca4e750e9f33a01b7d0707e3b4a43642313165
petersantoso94 <petersantoso94@gmail.com>
2018-07-18 21:04:53 +0800

    18-7-2018

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,14 +1,45 @@
 @extends('template.header-footer')
 @section('main-section')
 <!-- Page Content -->
-    <!-- Jumbotron Header -->
-    <!-- Page Features -->
-    <div class="row text-center" style="background: rgba(204, 204, 204, 0.8);;">
+<!-- Jumbotron Header -->
+<!-- Page Features -->
+<div class="row text-center" style="background: rgba(204, 204, 204, 0.8);margin-top: 20px;">
+    <div class='col-xs-4'>
         @if(Session::get('username') != null)
-        <p>Hi, {{Session::get('username')}}</p>
+        <h4>Hi, {{Session::get('username')}}</h4>
         @endif
+        <h3>Welcome to SEAL ONLINE: Chronicles of Shiltz</h3>
+        <?php
+        $a = Session::get('username');
+        $email = '';
+        $pin = '';
+        $letter = $a['0'];
+        $table = '';
+        if (preg_match("/[aA-dD0-9]/", $letter)) {
+            $table = "idtable1";
+        } else if (preg_match("/[eE-iI]/", $letter)) {
+            $table = "idtable2";
+        } else if (preg_match("/[eJ-nN]/", $letter)) {
+            $table = "idtable3";
+        } else if (preg_match("/[oO-sS]/", $letter)) {
+            $table = "idtable4";
+        } else if (preg_match("/[tT-zZ]/", $letter)) {
+            $table = "idtable5";
+        } else {
+            $table = "idtable5";
+        }
+        $registered_id = DB::connection('mysql')->table($table)->select('*')->where('id', $a)->get();
+        if (count($registered_id) > 0) {
+            $email = $registered_id[0]->email;
+            $pin = $registered_id[0]->trueId;
+        }
+        ?>
+        <p>Your ID: {{$username}}</p>
+        <p>Your Email: {{$email}}</p>
+        <p>Your PIN:</p><input type='password' value='{{$pin}}' disabled="">
     </div>
-    <!-- /.row -->
+</div>
+<!-- /.row -->
 
 </div>
 <!-- /.container -->
