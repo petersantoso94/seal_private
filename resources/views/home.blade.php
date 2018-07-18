dfd6947750d3a8b565f739872f4747bf396cbb10
petersantoso94 <petersantoso94@gmail.com>
2018-07-18 23:09:45 +0800

    18-7-2018

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
         Successfully resetting your password
     </div>
     @else
-    <div class="alert alert-error alert-dismissible" role="alert" style="width: 98%; margin: 1%">
+    <div class="alert alert-danger alert-dismissible" role="alert" style="width: 98%; margin: 1%">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         Wrong pin number
     </div>
