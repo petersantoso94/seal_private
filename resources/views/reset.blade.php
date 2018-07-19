ef81247892ccc066fa71322bfe6d9764a0ccb87f
peter <petersantoso94@gmail.com>
2018-07-19 16:53:58 +0800

    19/7/2018 forget pass

--- resources/views/reset.blade.php
+++ resources/views/reset.blade.php
@@ -18,7 +18,7 @@
                             @else
                             <div class="alert alert-danger alert-dismissible" role="alert" style="width: 98%; margin: 1%">
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                Wrong pin number
+                                Data does not match!
                             </div>
                             @endif
                         <?php } ?>
