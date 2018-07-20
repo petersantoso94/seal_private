ebeb7045b5241f995f27f5735aad7ba13efc2aa7
petersantoso94 <petersantoso94@gmail.com>
2018-07-20 22:14:03 +0800

    20-7-2018

--- resources/views/fanart.blade.php
+++ resources/views/fanart.blade.php
@@ -6,7 +6,7 @@
 <div class="row" style="background: rgba(204, 204, 204, 0.8);margin-top: 20px;padding-left: 10px;padding-right: 10px">
     @foreach(DB::connection('mysql2')->table('fanart')->orderBy('id', 'DESC')->select('*')->get() as $img)
     <div class="col-md-4">
-        <img class="card-img-top" src="{{URL::asset('public/picture/fanart/'.$img->image)}}" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+        <img class="card-img-top" src="{{URL::asset('public/picture/'.$img->image)}}" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
     </div>
     @endforeach
 </div>
