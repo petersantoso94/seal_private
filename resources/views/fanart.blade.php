fc1de40c8b419fb7706112477a683eb8c9b72c8f
petersantoso94 <petersantoso94@gmail.com>
2018-08-17 23:54:05 +0800

    17-8-2018

--- resources/views/fanart.blade.php
+++ resources/views/fanart.blade.php
@@ -4,7 +4,7 @@
 <!-- Jumbotron Header -->
 <!-- Page Features -->
 <div class="row" style="background: rgba(204, 204, 204, 0.8);margin-top: 20px;padding-left: 10px;padding-right: 10px">
-    @foreach(DB::connection('mysql2')->table('fanart')->where('approved',1)->orderBy('id', 'DESC')->select('*')->get() as $img)
+    @foreach(DB::connection('mysql2')->table('fanart')->where('approved','1')->orderBy('id', 'DESC')->select('*')->get() as $img)
     <div class="col-md-4">
         <img class="card-img-top" src="{{URL::asset('public/picture/'.$img->image)}}" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
     </div>
