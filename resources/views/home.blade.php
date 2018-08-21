61fc8e80c7468e8aa85a0b60002289d69b878a1b
peter <petersantoso94@gmail.com>
2018-08-21 14:23:26 +0800

    21/8/2018 slider youtube

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,13 +34,13 @@
             <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
         </ol>
         <div class="carousel-inner" style="height:500px; width: 100%;">
+            <div class="carousel-item img-container-cs" style='width: 100%;'>
+                <iframe  width="1018" height="500" src="https://www.youtube.com/embed/ycYLA_6-FTk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
             <div class="carousel-item img-container-cs active">
                 <img class="d-block w-100 img-slider" src="{{URL::asset('public/picture/Home Logo Seal CoS.png')}}" alt="First slide" >
                 <!--                <div class="carousel-caption d-none d-md-block" style="background-color:rgba(211,211,211,0.5);color:black;">     
                                 </div>-->
-            </div>
-            <div class="carousel-item img-container-cs d-block" style='width: 100%;'>
-                <iframe  width="100%" height="500" src="https://www.youtube.com/embed/ycYLA_6-FTk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
             </div>
             <div class="carousel-item img-container-cs" >
                 <img class="d-block w-100 img-slider" src="{{URL::asset('public/picture/Pet 6th.png')}}" alt="Second slide" >
