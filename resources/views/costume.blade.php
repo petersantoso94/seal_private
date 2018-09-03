5b1de0c405357d7d58853c2b20768457791f043a
petersantoso94 <petersantoso94@gmail.com>
2018-09-03 22:29:07 +0800

    3-9-2018

--- resources/views/costume.blade.php
+++ resources/views/costume.blade.php
@@ -3,7 +3,7 @@
 <!-- Page Content -->
 <!-- Jumbotron Header -->
 <!-- Page Features -->
-<div class="row" style="background: rgba(204, 204, 204, 0.8);margin-top: 20px;padding-left: 10px;padding-right: 10px">
+<div class="row" style="background: rgba(204, 204, 204, 0.8);margin-top: 20px;">
     @foreach(DB::connection('mysql2')->table('costume')->where('approved','1')->orderBy('id', 'DESC')->select('*')->get() as $img)
     <div class="col-md-4" style="background: rgba(255, 255, 255, 0.7); margin: 2px">
         <img class="card-img-top" src="{{URL::asset('public/picture/'.$img->image)}}" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
