02133777881f2ca71f060a03a4e5669a794cca6b
petersantoso94 <petersantoso94@gmail.com>
2018-09-03 22:26:54 +0800

    3-9-2018

--- resources/views/costume.blade.php
+++ resources/views/costume.blade.php
@@ -5,7 +5,7 @@
 <!-- Page Features -->
 <div class="row" style="background: rgba(204, 204, 204, 0.8);margin-top: 20px;padding-left: 10px;padding-right: 10px">
     @foreach(DB::connection('mysql2')->table('costume')->where('approved','1')->orderBy('id', 'DESC')->select('*')->get() as $img)
-    <div class="col-md-4">
+    <div class="col-md-4" style="background: rgba(255, 255, 255, 0.8);">
         <img class="card-img-top" src="{{URL::asset('public/picture/'.$img->image)}}" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
         <div style="height: 30px; width: 100%; display: block;">
             <p>{{$img->caption}}</p>
